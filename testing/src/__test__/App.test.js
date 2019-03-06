4d8da4b535515f827e18e3ffbd5a2c51830d5b58
Matthew Dodi <matthew.dodi@gmail.com>
2019-03-05 22:53:29 -0800

    Type: feat - Added second test - Extracted wrapped variable outside of the tests so that it's reusable

--- testing/src/__test__/App.test.js
+++ testing/src/__test__/App.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../components/CommentBox';
+import CommentList from '../components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
